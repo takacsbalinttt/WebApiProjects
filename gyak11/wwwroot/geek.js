6d287086a408dd8543555ba0142ab47c09992501
takacsbalinttt <balint.takacs5@stud.uni-corvinus.hu>
2023-11-27 15:00:04 +0100

    Gyak11

---
+++ gyak11/wwwroot/geek.js
@@ -0,0 +1,13 @@
+﻿console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
+
+
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
